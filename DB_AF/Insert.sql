828a03253f842f7155979ea64b191ff508c4651b
Genny <gennarocecco122@gmail.com>
2022-02-08 19:16:05 +0100

    Added Popolazione.java and jmetal gradle dependecy

--- DB_AF/Insert.sql
+++ DB_AF/Insert.sql
@@ -7,6 +7,18 @@ INSERT INTO utenti ( `Nome`, `Cognome`, `Pwd`, `Mail`, `Ruolo`) VALUES ('Domenic
 INSERT INTO utenti ( `Nome`, `Cognome`, `Pwd`, `Mail`, `Ruolo`) VALUES ('Mario', 'Rossi', 'lol', 'MarioDraghi@libero.it', '2');	/*Dipendente*/
 INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ( 'HTML', 'HTML');
 INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('CSS', 'CSS');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('Java', 'Java');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('C', 'C');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('Python', 'Python');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('React', 'React');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('Node', 'Node');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('C++', 'C++');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('javascript', 'javascript');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('Ruby', 'Ruby');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('C#', 'C#');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('Android', 'Android');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('SQL', 'SQL');
+INSERT INTO skill (`NomeSkill`, `DescrizioneSkill`) VALUES ('PHP', 'PHP');
 INSERT INTO team (`NomeProgetto`, `NumeroDipendenti`, `NomeTeam`, `Descrizione`, `IdTM`) VALUES ('TechAll', '8', 'NuoveTecnologie', 'Ricerchiamo nuove tecnologie', 3);
 INSERT INTO team (`NomeProgetto`, `NumeroDipendenti`, `NomeTeam`, `Descrizione`, `Competenza`, `IdTM`) VALUES ('NewTech', '1', 'Arte', 'Nuove tecnologie biomeccaniche', 'Basi di Ingegneria Meccanica', 3);
 INSERT INTO dipendenti (`IdDipendente`,`Residenza`, `Telefono`, `Stato`, `AnnoDiNascita`, `IdTeam`) VALUES (2,'Fisciano', '118', '0', '2000', '1');
